1e6ff133ab5396baab73ee64e02bf7e9e096249d
vksilambu14-art <vksilambu14@gmail.com>
2026-06-17 21:53:10 +0530

    First Task Completed

---
+++ Taskone_basic/Basic.js
@@ -0,0 +1,32 @@
+var name="Silambu V";
+
+console.log("Employee Name: " + name);
+
+var age=25;
+
+console.log("Employee Age: " + age);
+
+const companyname="Stackly";
+
+console.log("Employee Company: " + companyname);
+
+
+let message="Welcome to Employee Portal";
+
+alert(message);
+
+let answer="Have you completed today's task?";
+console.log(confirm(answer));
+
+console.log(prompt("Enter your name:-"));
+
+let Employeename="Welcome Silambu V";
+document.writeln(Employeename);
+
+console.warn("Task submitted successfully");
+
+console.error("Task submission failed");
+
+let employeage=25;
+ employeage=45;
+console.log(employeage);
